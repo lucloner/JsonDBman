453e8b7ee1f070d1870152a09c90a8894a2e48e6
Biggee <lucloner@hotmail.com>
2019-04-18 10:32:52 +0800

    增加死循环检测，增加停止功能 1

--- src/net/vicp/biggee/kotlin/json/DBHelper.kt
+++ src/net/vicp/biggee/kotlin/json/DBHelper.kt
@@ -6,30 +6,55 @@ import com.google.gson.JsonObject
 import net.vicp.biggee.kotlin.db.JsonHelper
 
 object DBHelper {
-    fun getLatestJsonArrayFromDB(tableName: String, key: String): JsonArray {
+    fun getLatestJsonArrayFromDB(tableName: String, vararg keys: String): JsonArray {
         val jsonArray = JsonHelper.getJsonArray(tableName)
-        val returnArray = getLatestJsonArray(jsonArray, key)
-        DBCache.collectCache(returnArray.toString(), tableName, key)
+        val returnArray = getLatestJsonArray(jsonArray, *keys)
+        DBCache.collectCache(returnArray.toString(), tableName, *keys)
         return returnArray
     }
 
-    private fun getLatestJsonArray(jsonArray: JsonArray, key: String): JsonArray {
+    fun getJsonSafe(tableName: String, vararg keys: String): JsonElement {
+        DBCache.cache_loadDBpath.clear()
+        val jsonArray = JsonArray()
+        val t = Thread {
+            jsonArray.addAll(getLatestJsonArrayFromDB(tableName, *keys))
+        }.apply {
+            setUncaughtExceptionHandler { t, e ->
+                e.printStackTrace()
+                if (e is LinkageError) {
+                    DBCache.stop()
+                }
+            }
+            isDaemon = true
+            start()
+        }
+        while (t.isAlive) {
+            Thread.sleep(100)
+        }
+        return jsonArray
+    }
+
+    //根据关键字,值相同的取最新的一个
+    private fun getLatestJsonArray(jsonArray: JsonArray, vararg keys: String): JsonArray {
         val returnArray = JsonArray()
         val latest = HashMap<String, JsonElement>()
-        jsonArray.iterator().forEach {
-            if (it is JsonObject) {
-                try {
-                    val value = it[key].asString
-                    latest.put(value, it)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    System.out.println("没有找到关键字:$key")
+        keys.iterator().forEach {
+            val key = it
+            jsonArray.iterator().forEach {
+                when (it) {
+                    is JsonObject -> try {
+                        val value = it[key].asString
+                        latest.put(value, it)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        System.out.println("没有找到关键字:$keys")
+                    }
+                    is JsonArray -> {
+                        val subArray = getLatestJsonArray(it, key)
+                        returnArray.add(subArray)
+                    }
+                    else -> latest.put("", it)
                 }
-            } else if (it is JsonArray) {
-                val subArray = getLatestJsonArray(it, key)
-                returnArray.add(subArray)
-            } else {
-                latest.put("", it)
             }
         }
         latest.values.iterator().forEach {
